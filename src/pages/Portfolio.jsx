081ecf4501037494dabd3e25fa138248813ef4d2
Zuheib Dahir Musse <112723729+hadeslaw@users.noreply.github.com>
2023-09-02 16:01:11 +0100

    Add files via upload

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import '../App.css';
+import logo from './UnderCon.png'
 
 
+function Header(){
+    return <img src={logo} alt ="Logo" />;
+}
 export default function Portfolio(){
     return <h1 className="Portfolio">Portfolio</h1>;
 }
