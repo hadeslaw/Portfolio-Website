827ea12148be3494f17a0b53b6c7c85b3ea3fe34
Zuheib Dahir Musse <112723729+hadeslaw@users.noreply.github.com>
2023-12-14 19:30:55 +0000

    Add files via upload

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import '../App.css';
-import logo from './UnderCon.png'
+import './Portfolio.css';
+//import logo from './UnderCon.png'
 
 
 function Header(){
-    return <img src={logo} alt ="Logo" />;
-}
+    <div class="card">
+
+        <div class="container">
+            <h4><b>Siqqe Solar</b></h4>
+            <p> This is the Frontend of an Ecomerce Website.
+                *work in progress*
+            </p>
+        </div>
+    </div>}
 export default function Portfolio(){
     return Header();
 }
